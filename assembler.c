33105ab409a48910cf9d219c618b80fc832b5602
jennaolson <jennaolson@github.com>
2017-10-15 14:36:48 -0500

    Updated error checking

--- assembler.c
+++ assembler.c
@@ -49,32 +49,32 @@ int main(int argc, char **argv)
 	    char *checker7 = strstr(token, "7");
 	    char *checker8 = strstr(token, "8");
 	    char *checker9 = strstr(token, "9");
-	    char *checkerspecialchar1 = strstr(token, "!");
-            char *checkerspecialchar2 = strstr(token, "@");
-  	    char *checkerspecialchar3 = strstr(token, "#");
-  	    char *checkerspecialchar4 = strstr(token, "$");
-  	    char *checkerspecialchar5 = strstr(token, "%");
-  	    char *checkerspecialchar6 = strstr(token, "^");
-  	    char *checkerspecialchar7 = strstr(token, "&");
-  	    char *checkerspecialchar8 = strstr(token, "*");
-  	    char *checkerspecialchar9 = strstr(token, "?");
-  	    char *checkerspecialchar10 = strstr(token, "<");
-  	    char *checkerspecialchar11 = strstr(token, ">");
-
 	    while (token) {
                 if(mark != markold) {
                     //check if first token is a label
                     if(strcmp(token, "beq") != 0 && strcmp(token, "add") != 0 && strcmp(token, "nand") && strcmp(token, "lw")!= 0 && strcmp(token, "sw") != 0 && strcmp(token, "jalr")!=0 && strcmp(token, "halt") != 0 && strcmp(token, "noop")!= 0 && strcmp(token, ".fill") != 0) {
-                        // Checks for invalid labels
-			if(checker0 == token || checker1 == token || checker2 == token || checker3 == token || checker4 == token || checker5 == token || checker6 == token || checker7 == token || checker8 == token || checker9 == token|| checkerspecialchar1 == token || checkerspecialchar2 == token || checkerspecialchar3 == token || checkerspecialchar4 == token || checkerspecialchar5 == token || checkerspecialchar6 == token || checkerspecialchar7 == token || checkerspecialchar8 == token || checkerspecialchar9 == token || checkerspecialchar10 == token || checkerspecialchar11 == token)
+                        // Checking for invalid labels -- Beginning with a number
+			if(checker0 == token || checker1 == token || checker2 == token || checker3 == token || checker4 == token || checker5 == token || checker6 == token || checker7 == token || checker8 == token || checker9 == token)
 			{
-				fprintf(stderr, "Label cannot begin with a number, or contain symbols.\n");
+				fprintf(stderr, "Label cannot begin with a number.\n");
 				return 0;
 			}
-			// Checks for duplicates
+			// Checking for invalid labels -- Containing special symbols
+			if(strstr(token, "!") != NULL || strstr(token, "@") != NULL || strstr(token, "#") != NULL || strstr(token, "$") != NULL || strstr(token, "%") != NULL || strstr(token, "^") != NULL || strstr(token, "&") != NULL || strstr(token, "*") != NULL || strstr(token, "(") != NULL || strstr(token, ")") != NULL || strstr(token, "<") != NULL || strstr(token, ">") != NULL || strstr(token, "?") != NULL || strstr(token, "+") != NULL || strstr(token, "/") != NULL)
+			{
+				fprintf(stderr, "Label cannot contain special symbols.\n");
+				return 0;
+			}
+			// Checking for invalid labels -- Duplicates
 			if(sm_exists(sm, token) == 1)
 			{
 				fprintf(stderr, "Duplicate labels cannot be entered.\n");
+				return 0;
+			}
+			// Checking for invalid labels -- More than 6 characters
+			if(strnlen(token, 32) > 6)
+			{
+				fprintf(stderr, "Label cannot be more than 6 characters.\n");
 				return 0;
 			}
 			// Checks for invalid opcodes -- not sure why this breaks your test2.txt
